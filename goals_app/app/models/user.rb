4c3485e65ae40067e61cf459605e194f37c16f16
yakzaetz <yakzaetz@gmail.com>
2022-01-19 14:43:45 -0500

    user auth methods

---
+++ goals_app/app/models/user.rb
@@ -0,0 +1,40 @@
+class User < ApplicationRecord
+
+    after_initialize :ensure_session_token
+
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password, length: {minimum: 6 allow_nil: true}
+    validates :password_digest, presence: true
+
+    attr_reader :password
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+
+        return nil if user.nil?
+        user.is_password?(password) ? user : nil
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    # def self.generate_session_token
+    #     self.session_token = SecureRandom.base64(64)
+    # end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.base64(64)
+        self.save!
+        self.session_token
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom.base64(64)
+    end
+end
